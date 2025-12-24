40239c582bb9c472ca24670360d30761a2f134b7
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-12-24 18:14:49 +0330

    feat: update Home page layout
    - Add Hero component below Navbar
    - Wrap Navbar and Hero in separate sections with proper IDs
    - Apply container class for consistent spacing

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,14 @@
 import Navbar from "./_components/navbar/Navabar";
+import Hero from "./_components/hero/Hero";
 export default function Home() {
-  return <Navbar />;
+  return (
+    <div>
+      <section id="sec-1" className="container mx-auto">
+        <Navbar />
+      </section>
+      <section id="sec-2">
+        <Hero />
+      </section>
+    </div>
+  );
 }
